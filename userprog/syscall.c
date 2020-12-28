58de26488fe0d0d39408cedde96afc2ae61e3aee
j5-jarvis <joseph2.jarvis@live.uwe.ac.uk>
2020-12-28 10:17:46 -0500

    Inital implementation of systemcalls halt & exit

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -17,5 +17,41 @@ static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
-  thread_exit ();
+  
+  int system_call_number = *((int*)f->esp); //
+  printf("system_call_handler() - %d! \n", system_call_number);
+
+	switch (system_call_number){
+
+	// Halts Pintos by calling the halt function, seen below
+	case SYS_HALT:
+	printf("SYSTEM CALL: Halt is being executed \n");
+	halt();
+	break;
+
+	/* Terminates the current user program, whilst returning the kernal status */
+	case SYS_EXIT:
+	printf("SYSTEM CALL: Exit is being executed \n");
+	
+	int status = *((int*)f->esp+1);
+	
+	struct thread* cur = thread_current();
+
+ 	cur->exit_code = status; // or exit_code.
+ 	
+	thread_exit();
+	break;
+  
+
+	default:
+	printf("Error! no system call was implemented");
+	thread_exit();
+	}
+
 }
+
+// halt function terminates Pintos
+void halt(void){
+shutdown_power_off();
+}
+
