c2b34ca0846483c9da630e1b60ac7022110555ef
j5-jarvis <joseph2.jarvis@live.uwe.ac.uk>
2021-01-18 13:19:47 -0500

    Testing Syscall exec

--- examples/my.c
+++ examples/my.c
@@ -7,18 +7,18 @@ main (void)
     //printf("Hello, World\n");
 	//halt();
 	//exit(0);
-	//exec();
+	exec("echo Test Tester Testing");
 	//wait();
-	char *file = "Test File";
-	create(file,10);
+	//char *file = "Test File";
+	//create(file,10);
 	//remove(file);
-	open(file);
+	//open(file);
 	//filesize(2);
         
-        void *buffer = "Hello";
-	write(2,buffer,5);
-	void *buffer_r = "";	
-	read(2,buffer_r,4);
+        //void *buffer = "Hello";
+	//write(2,buffer,5);
+	//void *buffer_r = "";	
+	//read(2,buffer_r,4);
 	
 	//seek(2,3);
 	//tell(2);
